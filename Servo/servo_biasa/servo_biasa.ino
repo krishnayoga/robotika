c855f45c4f6ebac3e04a066f7a14f7218450163e
krishnayoga <ida.bagus52@ui.ac.id>
2018-12-26 21:12:21 +0700

    edit servo

---
+++ Servo/servo_biasa/servo_biasa.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int derajat;
+int incomingByte = 0;
+
+void setup(){
+  Serial.begin(9600);
+  myservo.attach(9);
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    incomingByte = Serial.read();
+
+    Serial.print("incoming: ");
+    Serial.print(incomingByte);
+
+    derajat = incomingByte;
+
+    myservo.write(derajat);
+  }
+  
+}
+
+
---
+++ Servo/servo_continuous/servo_continuous.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo myservo; 
+ 
+int incomingByte = 0;  
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);  
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    
+    Serial.print("incoming: ");
+    Serial.print (incomingByte);
+    if(incomingByte == 100){
+      Serial.println(" CW 0 derajat "); 
+      myservo.write(0); 
+    }
+    else if(incomingByte == 200){
+      Serial.println(" CCW 180 derajat "); 
+      myservo.write(180); 
+    }
+    else if(incomingByte == 300){
+      Serial.println(" Stop "); 
+      myservo.write(60); 
+    }
+    else{
+      Serial.println(" Jalan ngaco "); 
+      myservo.write(incomingByte); 
+    }
+  }
+} 
